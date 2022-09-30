cc7e1f02413f955dec5e96dedd557218d80410bd
yamaslena@yandex.ru <yamaslena@yandex.ru>
2022-09-30 21:18:50 +0700

    fetch, useEffect,mockapi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React from 'react';
 import './scss/app.scss';
 
 import Header from './components/Header';
 import Sort from './components/Sort';
 import Categories from './components/Categories';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
+// import pizzas from './assets/pizzas.json';
 
 function App() {
+  // https://6336e2ec5327df4c43cb898a.mockapi.io/items
+
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://6336e2ec5327df4c43cb898a.mockapi.io/items')
+      .then((res) => {
+        // console.log(res);
+        return res.json();
+      })
+      .then((arr) => {
+        // console.log('массив пицц', arr);
+        setItems(arr);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -18,9 +35,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock
-              key={obj.id }
+                key={obj.id}
                 {...obj}
                 // title={obj.title}
                 // price={obj.price}
